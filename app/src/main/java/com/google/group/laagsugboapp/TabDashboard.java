d86ae73661282a8fe40350e8502494ab50007c0f
coroyoy1 <you@example.com>
2019-02-01 20:29:13 +0800

    tabbed

---
+++ app/src/main/java/com/google/group/laagsugboapp/TabDashboard.java
@@ -0,0 +1,18 @@
+package com.google.group.laagsugboapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TabDashboard extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tabdashboard, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/google/group/laagsugboapp/TabMyPost.java
@@ -0,0 +1,16 @@
+package com.google.group.laagsugboapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TabMyPost extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tabmypost, container, false);
+        return rootView;
+    }
+}
