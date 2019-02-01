df55aafe02f31516a96048a1f619a0f7e6fe7e03
coroyoy1 <you@example.com>
2019-02-01 19:51:31 +0800

    tabbed

--- app/src/main/java/com/google/group/laagsugboapp/LaagSugboTabbedActivity.java
+++ app/src/main/java/com/google/group/laagsugboapp/LaagSugboTabbedActivity.java
@@ -118,7 +118,7 @@ public class LaagSugboTabbedActivity extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_laag_sugbo_tabbed, container, false);
+            View rootView = inflater.inflate(R.layout.tabdashboard, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
             textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
